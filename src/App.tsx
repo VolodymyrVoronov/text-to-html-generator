3b086380f577942d70889cb7247ca63f5bcd3a65
Volodymyr Voronov <malones.vladimir@gmail.com>
2021-12-17 19:16:42 +0100

    chore(App): added detection for mobile devices.

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,65 @@
 import { FC, Suspense, lazy } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
+import {
+  isMobile,
+  isWearable,
+  isAndroid,
+  isIOS,
+  isWinPhone,
+} from "react-device-detect";
 
 import Paths from "const/path";
 
 import StartPage from "pages/StartPage/StartPage";
+import Typography from "components/Typography/Typography";
 
 const EditorPage = lazy(
   () => import(/* webpackPrefetch: true */ "pages/EditorPage/EditorPage")
 );
 
 const App: FC<{}> = (): JSX.Element => {
+  const isMobileDevise =
+    isMobile || isWearable || isAndroid || isIOS || isWinPhone;
+
   return (
-    <Suspense fallback={<p>Loading</p>}>
-      <Routes>
-        <Route path={Paths.Root} element={<Navigate to={Paths.StartPage} />} />
-        <Route path={Paths.StartPage} element={<StartPage />} />
-        <Route path={Paths.EditorPage} element={<EditorPage />} />
-      </Routes>
-    </Suspense>
+    <>
+      {isMobileDevise ? (
+        <Typography
+          styles={{
+            display: "flex",
+            justifyContent: "center",
+            marginTop: 50,
+            fontSize: "24px",
+          }}
+        >
+          Only on desktop/laptop.
+        </Typography>
+      ) : (
+        <Suspense
+          fallback={
+            <Typography
+              styles={{
+                display: "flex",
+                justifyContent: "center",
+                marginTop: 50,
+                fontSize: "24px",
+              }}
+            >
+              Loading...
+            </Typography>
+          }
+        >
+          <Routes>
+            <Route
+              path={Paths.Root}
+              element={<Navigate to={Paths.StartPage} />}
+            />
+            <Route path={Paths.StartPage} element={<StartPage />} />
+            <Route path={Paths.EditorPage} element={<EditorPage />} />
+          </Routes>
+        </Suspense>
+      )}
+    </>
   );
 };
 
